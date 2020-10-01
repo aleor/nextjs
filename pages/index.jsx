10b77b870d658babe8f08e49aa849ef48646f5e4
Rustem Akhmetov <aleor2015@gmail.com>
2020-10-01 12:09:04 +0200

    getStaticProps

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,7 @@
 /** @jsx jsx */
 import { jsx } from 'theme-ui';
 
-const IndexPage = () => (
+const IndexPage = ({ content }) => (
   <div sx={{ height: `calc(100vh - 60px)` }}>
     <div
       sx={{
@@ -11,9 +11,21 @@ const IndexPage = () => (
         height: '100%',
       }}
     >
-      <h1 sx={{ fontSize: 8, my: 0 }}>Note taking app</h1>
+      <h1 sx={{ fontSize: 8, my: 0 }}>{content.title}</h1>
     </div>
   </div>
 );
 
 export default IndexPage;
+
+export function getStaticProps() {
+  // get actual data here...
+
+  return {
+    props: {
+      content: {
+        title: 'NextJs note taking app',
+      },
+    },
+  };
+}
--- pages/notes/[id].jsx
+++ pages/notes/[id].jsx
@@ -1,16 +1,34 @@
 /** @jsx jsx */
 import { jsx } from 'theme-ui';
-import { useRouter } from 'next/router';
 
-const Note = () => {
-  const router = useRouter();
-  const { id } = router.query;
-
+const Note = ({ note }) => {
   return (
     <div sx={{ variant: 'containers.page' }}>
-      <h1>Note: {id} </h1>
+      <h1>
+        {note.title}: {note.id}{' '}
+      </h1>
     </div>
   );
 };
 
 export default Note;
+
+export async function getServerSideProps({ params, req, res }) {
+  const response = await fetch(`http://localhost:3000/api/note/${params.id}`);
+
+  if (!response.ok) {
+    res.writeHead(302, { Location: '/notes' });
+
+    res.end();
+
+    return {
+      props: {},
+    };
+  }
+
+  const { data } = await response.json();
+
+  return {
+    props: { note: data },
+  };
+}
